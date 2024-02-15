50ff90f430f64af0d50ba11c6def286665a78be8
Eggcanoe <tommyhalleland@hotmail.com>
2024-02-15 09:31:58 +0100

    Base finished

--- stateArray/src/App.jsx
+++ stateArray/src/App.jsx
@@ -1,10 +1,24 @@
 import React from "react";
 
 function App() {
+  //Sets the state
+  const [thingsArray, setThingsArray] = React.useState(["Thing 1", "Thing 2"]);
+
+  //This function adds a new item to the list by re-rendering the list with the memory of the last state the list was in and adding one more "Thing".
+  function addItem() {
+    setThingsArray((prevThingsArray) => {
+      return [...prevThingsArray, `Thing ${prevThingsArray.length + 1}`];
+    });
+  }
+
+  //Adds a paragraph for each item from thingsArray and displays it under the add item button
+  const thingsElements = thingsArray.map((thing) => <p key={thing}>{thing}</p>);
+
   return (
-    <>
-      <h1>Test</h1>
-    </>
+    <div>
+      <button onClick={addItem}>Add Item</button>
+      {thignsElements}
+    </div>
   );
 }
 
